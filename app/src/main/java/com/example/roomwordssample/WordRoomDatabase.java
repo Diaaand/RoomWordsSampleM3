fb9cfbbf2ad725292c58ff07abea2629f75cdb3a
diaaand <diaaanaaad@gmail.com>
2022-06-25 22:40:53 +0700

    update2

--- app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -1,4 +1,56 @@
 package com.example.roomwordssample;
 
-public class WordRoomDatabase {
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.AsyncQueryHandler;
+import android.content.Context;
+import android.os.AsyncTask;
+
+@Database(entities = (Word.class), version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+    public static WordRoomDatabase getDatabase(final Context context) {
+        if(INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if(INSTANCE == NULL) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database")
+
+                            .fallbackToDestructiveMigration()
+                            .addCallback(SRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = onOpen(db) -> {
+        super.onOpen(db);
+        new PopulateDbAsync(INSTANCE).execute();
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+        String[] words = {"Tea", "Coffe", "Soda"};
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+
+            for(int i = o; i <= words.length-1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
+    }
 }
